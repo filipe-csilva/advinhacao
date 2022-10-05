0709490d0334d2ce82dd6583b40688b65ade21d0
filipeleasing <filipepaulocs@gmail.com>
2022-10-05 20:29:13 -0300

    1.0.1

---
+++ jogoadivinhacao.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <string.h>
+
+using namespace std;
+
+int main (){
+
+    cout << "***************************************"<< endl;
+    cout << "*  Bem-Vindos ao jogo da adivinhação  *"<< endl;
+    cout << "***************************************"<< endl;
+
+}
